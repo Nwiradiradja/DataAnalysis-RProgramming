41a2c9514a5e511a90e1bf53f49f236af96f5502
Nwiradiradja <NateWiradiradja@gmail.com>
2023-07-24 23:58:31 -0400

    Correlation

---
+++ CORRELATION.R
@@ -0,0 +1,51 @@
+
+###Correlation refers to the statistical concept that studies the relationship
+#### between 2 quantitative variables.
+
+### INSTALL THE PACKAGES
+
+install.packages("corrplot")
+install.packages("ggcorrplot")
+
+#### LOAD THE PACKAGES
+
+library(ggplot2)
+library(corrplot)
+library(ggcorrplot)
+
+### LOAD THE DATASET
+
+store <- read.csv("RATINGS.csv")
+
+####FIND AND PLOT CORRELATION BETWEEN RATINGS AND QUANTITY DEMANDED
+
+cor(store$RATING, store$QUANTITY_DEMANDED)
+plot(store$RATING, store$QUANTITY_DEMANDED)
+
+####CORRELATION TEST
+### PEARSON'S CORRELATION 
+
+cor.test(store$RATING, store$QUANTITY_DEMANDED)
+
+###  CORRELATION MATRIX
+
+store <- read.csv("FINAL DEPARTMENTAL STORE.csv")
+
+#### CORRELATION MATRIX
+
+store<-dplyr::select_if(store, is.numeric)
+r<-cor(store, use="complete.obs")
+round(r,2)
+
+####PLOT THE CORRELATION MATRIX  
+###HEAT MAP
+
+ggcorrplot(r)
+
+#### PLOT THE SORTED LOWER TRIANGLE
+
+ggcorrplot(r, 
+       hc.order = TRUE,
+       type = "lower",
+       lab = TRUE)
+
